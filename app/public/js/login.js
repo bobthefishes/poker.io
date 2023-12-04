3f0ba8c99aff59f63c5a4e11c093799a211f2156
Glitch (poker-io-offline) <none>
2023-12-04 19:09:39 +0000

    Import of bobthefishes/poker.io

---
+++ app/public/js/login.js
@@ -0,0 +1,12 @@
+function submit_creds() {
+    document.querySelector(".invalid").innerHTML = "";
+    let username = document.querySelector(".uname").value;
+    let password = document.querySelector(".pwd").value;
+    socket.emit("log in",username,password);
+}
+socket.on("invalid details", (type) => {
+    document.querySelector(".invalid").innerHTML = `Invalid ${type}`
+})
+socket.on("logged in", () =>{
+    window.location.href = "poker";
+})
