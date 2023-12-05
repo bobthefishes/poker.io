3364ab50dc1619235ff740494b9d9a0c7455863d
Glitch (poker-io) <none>
2023-12-05 13:46:42 +0000

    🛬🍧 Checkpoint ./server.js:63361290/561

--- app/public/js/blackjack.js
+++ app/public/js/blackjack.js
@@ -1,35 +1,35 @@
 const suits = {
-  Hearts: 0,
-  Diamonds: 1950,
-  Clubs: 3900,
-  Spades: 5850,
-};
+  "Hearts": 0,
+  "Diamonds": 1950,
+  "Clubs": 3900,
+  "Spades": 5850
+}
 class Deck {
   constructor() {
-    this.deck = [];
-    for (const suit in suits) {
-      for (let i = 2; i < 15; i++) {
-        this.deck.push([i, suit]);
+      this.deck = [];
+      for (const suit in suits) {
+          for (let i = 2; i < 15; i++) {
+              this.deck.push([i, suit]);
+          }
       }
-    }
   }
   resetcards() {
-    this.deck = [];
-    for (const suit in suits) {
-      for (let i = 2; i < 15; i++) {
-        this.deck.push([i, suit]);
+      this.deck = [];
+      for (const suit in suits) {
+          for (let i = 2; i < 15; i++) {
+              this.deck.push([i, suit]);
+          }
       }
-    }
-    this.shufflecards();
+      this.shufflecards();
   }
   shufflecards() {
-    for (let i = this.deck.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
-    }
+      for (let i = this.deck.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
+      }
   }
   returncard() {
-    return this.deck.pop();
+      return this.deck.pop();
   }
 }
 
@@ -53,17 +53,10 @@ function turn_over_playerCard(card) {
   var next_card = game_deck.returncard();
   player_cards.push(next_card);
   card.style.backgroundImage = `url('../assets/newDeck/${next_card[1]}/card${next_card[1]}_${next_card[0]}.png')`;
-  card.style.backgroundSize = "100% 109%";
-  if (next_card[0] === 11 || next_card[0] === 12 || next_card[0] === 13) {
-    value = 10;
-  } 
-  else if (next_card[0] === 14) {
-    value = 11;
-    player_aces = player_aces + 1;
-  } 
-  else {
-    value = parseInt(next_card[0]);
-  }
+  card.style.backgroundSize = '100% 109%';
+  if (next_card[0] === 11 || next_card[0] === 12 || next_card[0] === 13) { value = 10 }
+  else if (next_card[0] === 14) { value = 11 }
+  else { value = parseInt(next_card[0]) };
   player_hand = player_hand + value;
 }
 function turn_over_dealerCard(card) {
@@ -72,17 +65,13 @@ function turn_over_dealerCard(card) {
   var next_card = game_deck.returncard();
   dealer_cards.push(next_card);
   card.style.backgroundImage = `url('../assets/newDeck/${next_card[1]}/card${next_card[1]}_${next_card[0]}.png')`;
-  card.style.backgroundSize = "100% 109%";
-  if (next_card[0] === 11 || next_card[0] === 12 || next_card[0] === 13) {
-    value = 10;
-  } 
+  card.style.backgroundSize = '100% 109%';
+  if (next_card[0] === 11 || next_card[0] === 12 || next_card[0] === 13) { value = 10 }
   else if (next_card[0] === 14) {
-    value = 11;
-    dealer_aces = dealer_aces + 1;
-  } 
-  else {
-    value = parseInt(next_card[0]);
+    value = 11
+    dealer_aces++;
   }
+  else { value = parseInt(next_card[0]) };
   dealer_hand = dealer_hand + value;
 }
 
@@ -90,17 +79,17 @@ function player_card() {
   if (!done_deal && done_bet) {
     done_deal = true;
     const Cards = document.querySelectorAll(".player_card");
-    Cards.forEach((card) => {
-      turn_over_playerCard(card);
-    });
+    Cards.forEach(card => {
+      turn_over_playerCard(card)
+    })
     const dealerCards = document.querySelectorAll(".dealer_card");
     var n = 0;
-    dealerCards.forEach((card) => {
+    dealerCards.forEach(card => {
       n++;
-      if (n === 1) {
+      if (n===1) {
         turn_over_dealerCard(card);
       }
-    });
+    })
     console.log(`player hand: ${player_hand}`);
     if (player_hand === 21) {
       stand();
@@ -113,74 +102,66 @@ function dealer_card() {
   if (done_bet) {
     const Cards = document.querySelectorAll(".dealer_card");
     var n = 0;
-    Cards.forEach((card) => {
+    Cards.forEach(card => {
       n++;
-      if (n === 2) {
-        turn_over_dealerCard(card);
+      if (n===2) {
+        turn_over_dealerCard(card)
       }
-    });
+    })
     console.log(`dealer hand: ${dealer_hand}`);
   }
 }
 
 function hide_stuff() {
-  if (stack > 0) {
-    document.querySelector(".play_again_btn").style.visibility = "visible";
-    document.querySelector(".betting_container").classList.add("remove");
-  }
-  else {
-    document.querySelector(".play_again_btn").style.visibility = "visible";
-    document.querySelector(".play_again_btn").innerHTML = 'Broke!'
-    document.querySelector(".betting_container").classList.add("remove");
-  }
+  document.querySelector(".play_again_btn").style.visibility = "visible";
+  document.querySelector(".betting_container").classList.add('remove');
 }
 
 function go_again() {
-  if (stack > 0) {
-    player_aces = 0;
-    dealer_aces = 0;
-    bet_input.disabled = false;
-    bet_input.value = "";
-    done_deal = false;
-    done_stand = false;
-    done_bet = false;
-    player_cards = [];
-    dealer_cards = [];
-    player_hand = 0;
-    dealer_hand = 0;
-    game_deck.resetcards();
-    game_deck.shufflecards();
-    num_hits = 2;
-    num_hits_dealer = 2;
-    pot = 0;
-    var n = 0;
-    var m = 0;
-    blackjack = false;
-    const playerCards = document.querySelectorAll(".player_card");
-    playerCards.forEach((card) => {
-      n++;
-      card.style.backgroundImage = "url(../assets/cardBackBlue.png)";
-      card.classList.remove("flip");
-      card.style.backgroundSize = "110% 110%";
-      if (n > 2) {
-        card.parentNode.removeChild(card);
-      }
-    });
-    const dealerCards = document.querySelectorAll(".dealer_card");
-    dealerCards.forEach((card) => {
-      m++;
-      card.classList.remove("flip");
-      card.style.backgroundImage = "url(../assets/cardBackRed.png)";
-      card.style.backgroundSize = "110% 110%";
-      if (m > 2) {
-        card.parentNode.removeChild(card);
-      }
-    });
-    document.querySelector(".betting_container").classList.remove("remove");
-    document.querySelector(".winner").innerHTML = "Winner:";
-    document.querySelector(".hit_btn").disabled = false;
-    document.querySelector(".play_again_btn").style.visibility = "hidden";
-  }
+  player_aces = 0;
+  dealer_aces = 0;
+  bet_input.disabled = false;
+  bet_input.value = '';
+  done_deal = false;
+  done_stand = false;
+  done_bet = false;
+  player_cards = [];
+  dealer_cards = [];
+  player_hand = 0;
+  dealer_hand = 0;
+  game_deck.resetcards();
+  game_deck.shufflecards();
+  num_hits = 2;
+  num_hits_dealer = 2;
+  pot = 0;
+  var n = 0;
+  var m = 0;
+  blackjack = false;
+  bust = false;
+  const playerCards = document.querySelectorAll(".player_card");
+  playerCards.forEach(card => {
+    n++;
+    card.style.backgroundImage = 'url(../assets/cardBackBlue.png)';
+    card.classList.remove('flip');
+    card.style.backgroundSize = "110% 110%";
+    if (n>2) {
+      card.parentNode.removeChild(card);
+    }
+  })
+  const dealerCards = document.querySelectorAll(".dealer_card");
+  dealerCards.forEach(card => {
+    m++;
+    card.classList.remove('flip');
+    card.style.backgroundImage = 'url(../assets/cardBackRed.png)';
+    card.style.backgroundSize = "110% 110%";
+    if (m>2) {
+      card.parentNode.removeChild(card);
+    }
+  })
+  document.querySelector(".betting_container").classList.remove('remove');
+  document.querySelector(".winner").innerHTML = 'Winner:';
+  document.querySelector(".hit_btn").disabled = false;
+  document.querySelector(".play_again_btn").style.visibility = 'hidden';
 }
 
 //betting
@@ -190,13 +171,13 @@ let done_stand = false;
 let blackjack = false;
 window.onload = function () {
   document.querySelector(".stack").innerHTML = `Your stack: £${stack}`;
-};
+}
 
 function handle_betInput() {
   let inputValue = document.querySelector(".bet_input").value;
-  inputValue = inputValue.replace(/[^0-9]/g, "");
+  inputValue = inputValue.replace(/[^0-9]/g, '');
   if (inputValue.length > 3) {
-    inputValue = inputValue.substring(0, 3);
+    inputValue = inputValue.substring(0,3);
   }
   document.querySelector(".bet_input").value = inputValue;
   bet = inputValue;
@@ -216,13 +197,15 @@ function make_bet() {
 function dealer_stand() {
   winner = get_winner(player_hand, dealer_hand);
   document.querySelector(".winner").innerHTML = `Winner: ${winner}`;
-  if (winner === "player" && blackjack === false) {
-    stack = stack + 2 * pot;
+  if (winner === 'player' && blackjack===false) {
+    stack = stack + 2*pot;
     document.querySelector(".stack").innerHTML = `Your stack: ${stack}`;
-  } else if (winner === "player" && blackjack === true) {
-    stack = stack + (pot + (3/2 * pot));
+  }
+  else if (winner==='player' && blackjack===true) {
+    stack = stack + 1.5*(2*pot);
     document.querySelector(".stack").innerHTML = `Your stack: ${stack}`;
-  } else if (winner === "push") {
+  }
+  else if (winner === 'push') {
     stack = stack + pot;
     document.querySelector(".stack").innerHTML = `Your stack: ${stack}`;
   }
@@ -235,74 +218,75 @@ function stand() {
     done_stand = true;
     if (player_hand <= 21) {
       dealer_hit();
-    } else {
-      dealer_stand();
     }
+    else {
+      dealer_stand()
+    };
     hide_stuff();
   }
-  document.querySelector(".hit_btn").disabled = true;
+    document.querySelector(".hit_btn").disabled = true;
 }
+
 
 let num_hits = 2;
 let num_hits_dealer = 2;
 
 function hit() {
   if (done_deal) {
-    var n = player_aces;
     num_hits++;
-    const newCard = document.createElement("div");
-    document.querySelector(".player_cards").appendChild(newCard);
-    newCard.classList.add("card");
-    newCard.classList.add("player_card");
+    const newCard = document.createElement('div')
+    document.querySelector('.player_cards').appendChild(newCard);
+    newCard.classList.add('card');
+    newCard.classList.add('player_card');
     newCard.classList.add(`card${num_hits}`);
     turn_over_playerCard(newCard);
-    
-    while (player_hand > 21 && player_aces > 0) {
-      player_hand -= 10;
-      player_aces -= 1;
-    }
-    if (player_hand === 21 || (player_hand > 21 && player_aces === 0)) {
+    if (player_hand === 21) {
       document.querySelector(".hit_btn").disabled = true;
       stand();
     }
-    console.log(`player hand: ${player_hand}`);
+    else if (player_hand > 21 && player_aces === 0) {
+      document.querySelector(".hit_btn").disabled = true;
+      stand();
+    }
+    else if (player_hand > 21 && player_aces > 0) {
+      for (let i=0; i<player_aces; i++) {
+        player_hand = player_hand - 10;
+        if (player_hand <= 21) {
+          document.querySelector(".hit_btn").disabled = true;
+          stand();
+          break;
+        }
+      }
+      if (player_hand > 21) {
+        document.querySelector(".hit_btn").disabled = true;
+        stand();
+      }
+    }
+    console.log(`player hand: ${player_hand}`)
   }
 }
 
 function dealer_hit() {
-  while (dealer_hand < 17 && player_hand <= 21) {
+  while (dealer_hand<17 && player_hand<=21) {
     num_hits_dealer++;
-    const newCard = document.createElement("div");
-    document.querySelector(".dealer_cards").appendChild(newCard);
-    newCard.classList.add("card");
-    newCard.classList.add("dealer_card");
+    const newCard = document.createElement('div')
+    document.querySelector('.dealer_cards').appendChild(newCard);
+    newCard.classList.add('card');
+    newCard.classList.add('dealer_card');
     newCard.classList.add(`card${num_hits_dealer}`);
     turn_over_dealerCard(newCard);
-    while (dealer_hand > 21 && dealer_aces > 0) {
-      dealer_hand -= 10;
-      dealer_aces -= 1;
-    }
-    if (dealer_hand === 21 || (dealer_hand > 21 && dealer_aces === 0)) {
-      document.querySelector(".hit_btn").disabled = true;
-      dealer_stand();
-    }
-    console.log(`dealer hand: ${dealer_hand}`);
-  }
+    console.log(`dealer hand: ${dealer_hand}`)
+  };
+  dealer_stand()
 }
 
 //winner
 function get_winner(a, b) {
   let winner;
-  if (a <= 21 && a > b) {
-    winner = "player";
-  } else if (a <= 21 && b > 21) {
-    winner = "player";
-  } else if (a === b && a + b <= 42) {
-    winner = "push";
-  } else if (b > a && b <= 21) {
-    winner = "dealer";
-  } else if (player_hand > 21) {
-    winner = "dealer";
-  }
+  if (a <= 21 && a > b) { winner = 'player' }
+  else if (a<=21 && b>21) { winner = 'player' }
+  else if (a === b && a+b <= 42) { winner = 'push' }
+  else if (b>a && b<=21) { winner = 'dealer' }
+  else if (player_hand > 21) { winner = 'dealer' }
   return winner;
 }
