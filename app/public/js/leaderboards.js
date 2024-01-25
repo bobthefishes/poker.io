e98f98dcd9e2d0d12d99a40986b6629215448857
bobthefishes <rkeelinggithub@gmail.com>
2024-01-25 20:56:06 +0000

    unordered leaderboards

--- app/public/js/leaderboards.js
+++ app/public/js/leaderboards.js
@@ -1,9 +1,27 @@
 const table = document.querySelector('.main_table');
+
 async function getData() {
-  socket.emit("return player winnings");
   return new Promise((resolve, reject) => {
-      socket.on("player winnings", (winnings)=>{
-        resolve(winnings);
-      });
+    socket.emit("return player winnings");
+    socket.on("player winnings", (winnings) => {
+      resolve(winnings);
+    });
   });
+}
+
+async function displayBoard() {
+  try {
+    const leaderBoard = await getData();
+    console.log(leaderBoard);
+    leaderBoard.forEach(player => {
+      var row = table.insertRow(-1);
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+
+      cell1.innerHTML = player[0];
+      cell2.innerHTML = player[1];
+    });
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
 }
